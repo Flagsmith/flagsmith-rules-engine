eeaffb42ee43c13b30723807859a0e73af90418c
kyle <kyle@solidstategroup.com>
2019-04-14 16:49:08 +0100

    Clear deps

--- example/src/lib/getProp.js
+++
@@ -1,9 +0,0 @@
-//getProp({}, ['x'], 'DEFAULT_VAL'
-export default function getProp( object, keys, defaultVal ){
-    keys = Array.isArray( keys )? keys : keys.split('.');
-    object = object[keys[0]];
-    if( object && keys.length>1 ){
-        return getProp( object, keys.slice(1));
-    }
-    return object === undefined? defaultVal : object;
-}
